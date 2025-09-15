eff757a86f18c56412071a91e97590358f0b1581
UrielARMX <148836440+UrielARMX@users.noreply.github.com>
2025-09-15 16:55:50 -0700

    Actualizacion de la clase Program
    Instancie la clase Arbol y la clase Nodo para insertar datos en el Arbol y recorrer el Arbol

--- _01_Arboles_Binarios/Program.cs
+++ _01_Arboles_Binarios/Program.cs
@@ -6,7 +6,31 @@ namespace _01_Arboles_Binarios
     {
         static void Main(string[] args)
         {
+            Arbol arbol = new Arbol();
 
+            Nodo raiz = arbol.InsertarNodo("a", null);
+
+            arbol.InsertarNodo("b", raiz);
+
+            arbol.InsertarNodo("c", raiz);
+
+            Nodo nuevo = arbol.InsertarNodo("d", raiz);
+            arbol.InsertarNodo("h", nuevo);
+
+            nuevo = arbol.InsertarNodo("e", raiz);
+            arbol.InsertarNodo("i", nuevo);
+            nuevo = arbol.InsertarNodo("j", nuevo);
+            arbol.InsertarNodo("p", nuevo);
+            arbol.InsertarNodo("q", nuevo);
+
+            nuevo = arbol.InsertarNodo("f", raiz);
+            arbol.InsertarNodo("k", nuevo);
+            arbol.InsertarNodo("l", nuevo);
+            arbol.InsertarNodo("m", nuevo);
+            nuevo = arbol.InsertarNodo("g", raiz);
+            arbol.InsertarNodo("n", nuevo);
+
+            arbol.TransversaPreO(raiz);
         }
     }
 }
