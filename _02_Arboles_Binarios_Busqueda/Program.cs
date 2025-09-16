a405f98df80d6994ddb01976f5998db78d64fb4e
UrielARMX <148836440+UrielARMX@users.noreply.github.com>
2025-09-15 19:33:48 -0700

    Actualizacion de la clase Program
    Instancie la clase Arbol y inserte algunos datos y utilice el metodo TransversaPreO para recorrer el arbol

--- _02_Arboles_Binarios_Busqueda/Program.cs
+++ _02_Arboles_Binarios_Busqueda/Program.cs
@@ -6,7 +6,25 @@ namespace _02_Arboles_Binarios_Busqueda
     {
         static void Main(string[] args)
         {
+            Arbol arbol = new Arbol();
 
+            Nodo raiz = arbol.InsertarNodo(6, null);
+            arbol.InsertarNodo(2, raiz);
+            arbol.InsertarNodo(8, raiz);
+            arbol.InsertarNodo(1, raiz);
+            arbol.InsertarNodo(4, raiz);
+            arbol.InsertarNodo(3, raiz);
+            arbol.InsertarNodo(5, raiz);
+            arbol.InsertarNodo(7, raiz);
+            arbol.InsertarNodo(11, raiz);
+            arbol.InsertarNodo(9, raiz);
+            arbol.InsertarNodo(0, raiz);
+            arbol.InsertarNodo(-1, raiz);
+            arbol.InsertarNodo(10, raiz);
+            arbol.InsertarNodo(12, raiz);
+            arbol.InsertarNodo(14, raiz);
+
+            arbol.TransversaPreO(raiz);
         }
     }
 }
