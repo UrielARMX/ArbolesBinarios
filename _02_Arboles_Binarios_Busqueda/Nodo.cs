421ca462ad7387a4658594e45d9f1e698da3f007
UrielARMX <148836440+UrielARMX@users.noreply.github.com>
2025-09-15 19:17:54 -0700

    Creacion de la clase Nodo
    Cree la clase Nodo con sus atributos, propiedades y constructor

---
+++ _02_Arboles_Binarios_Busqueda/Nodo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Arboles_Binarios_Busqueda
+{
+    class Nodo
+    {
+        private int dato;
+        private Nodo izq;
+        private Nodo der;
+
+        public int Dato { get => dato; set => dato = value; }
+        public Nodo Izq { get => izq; set => izq = value; }
+        public Nodo Der { get => der; set => der = value; }
+
+        public Nodo()
+        {
+            dato = 0;
+            izq = null;
+            der = null;
+        }
+    }
+}
