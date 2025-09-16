5a2816198a2fe949ef59827f507bc69627a36cab
UrielARMX <148836440+UrielARMX@users.noreply.github.com>
2025-09-15 19:26:53 -0700

    Actualizacion de la clase Arbol
    Cree las propiedades y atributos de la clase y cree el metodo insertar para insertar en el arbol

--- _02_Arboles_Binarios_Busqueda/Arbol.cs
+++ _02_Arboles_Binarios_Busqueda/Arbol.cs
@@ -8,5 +8,30 @@ namespace _02_Arboles_Binarios_Busqueda
 {
     class Arbol
     {
+        private Nodo raiz;
+        private Nodo Actual;
+        private int i = 0;
+
+        public Arbol() => raiz = null;
+
+        internal Nodo Raiz { get => raiz; set => raiz = value; }
+
+        public Nodo InsertarNodo(int dato, Nodo nodo)
+        {
+            if (nodo == null)
+            {
+                Raiz = new Nodo();
+                Raiz.Dato = dato;
+
+                Raiz.Izq = null;
+                Raiz.Der = null;
+
+                return Raiz;
+            }
+            else if (dato < nodo.Dato) nodo.Izq = InsertarNodo(dato, nodo.Izq);
+            else if (dato > nodo.Dato) nodo.Der = InsertarNodo(dato, nodo.Der);
+
+            return nodo;
+        }
     }
 }
